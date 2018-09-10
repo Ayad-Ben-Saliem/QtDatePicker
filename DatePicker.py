7b365e43f3789ca37a4325547c79254caaec4645
ayad <ayad@eng.misuratau.edu.ly>
2018-09-10 11:43:36 +0200

    add new functions, signals, slots to DatePicker

--- DatePicker.py
+++ DatePicker.py
@@ -1,19 +1,23 @@
 from PySide2.QtGui import QIcon
 from PySide2.QtCore import \
-    Qt,\
-    Slot,\
-    SLOT,\
-    QSize,\
-    QPoint
+    Qt, \
+    Slot, \
+    SLOT, \
+    QSize, \
+    QPoint, \
+    Signal
 from PySide2.QtWidgets import \
-    QWidget,\
-    QDialog,\
-    QPushButton,\
-    QHBoxLayout,\
+    QWidget, \
+    QDialog, \
+    QPushButton, \
+    QHBoxLayout, \
     QCalendarWidget
 
 
 class DatePicker(QWidget):
+
+    selectionChanged = Signal()
+
     def __init__(self, parent=None):
         super(DatePicker, self).__init__(parent)
         self.button = QPushButton(self)
@@ -34,7 +38,14 @@ class DatePicker(QWidget):
         self.dialog.layout().setContentsMargins(0, 0, 0, 0)
         self.dialog.layout().setSpacing(0)
 
-        self.button.clicked.connect(self, SLOT("showCalender()"))
+        self.button.clicked.connect(self.showCalender)
+        self.calender.selectionChanged.connect(self.__onSelectionChanged__)
+
+    def show(self):
+        self.showCalender()
+
+    def close(self):
+        self.dialog.close()
 
     @Slot()
     def showCalender(self):
@@ -54,44 +65,41 @@ class DatePicker(QWidget):
     def icon(self):
         return self.button.icon()
 
-
-
-    def setIconSize(self, iconsize):
-        if type(iconsize) is QSize:
-            self.button.setIconSize(iconsize)
-        elif type(iconsize) is int:
-            self.button.setIcon(QSize(iconsize, iconsize))
-        elif type(type) is iter:
-            import collections
-            if isinstance(iconsize, collections.Iterable):
-                if len(iconsize) == 1:
-                    self.setIconSize(iconsize[0])
-                elif len(iconsize) == 2:
-                    self.setIconSize(QSize(iconsize[0], iconsize[1]))
-                else:
-                    raise Exception()
-        else:
-            raise Exception("Wrong argument type, iconSize should be either PySide2.QtCore.QSize or int value or width and height "
-                            "or iterable contains one QSize, one int or two int values for width and height respectively")
+    def setIconSize(self, iconSize):
+        self.button.setIconSize(iconSize)
+        # if type(iconSize) is QSize:
+        #     self.button.setIconSize(iconSize)
+        # elif type(iconSize) is int:
+        #     self.button.setIcon(QSize(iconSize, iconSize))
+        # elif type(type) is iter:
+        #     import collections
+        #     if isinstance(iconSize, collections.Iterable):
+        #         if len(iconSize) == 1:
+        #             self.setIconSize(iconSize[0])
+        #         elif len(iconSize) == 2:
+        #             self.setIconSize(QSize(iconSize[0], iconSize[1]))
+        #         else:
+        #             raise Exception()
+        # else:
+        #     raise Exception("Wrong argument type, iconSize should be either PySide2.QtCore.QSize or int value or width and height "
+        #                     "or iterable contains one QSize, one int or two int values for width and height respectively")
 
     def iconSize(self):
         return self.button.iconSize()
 
-
-
-    def setFirstDayOfWeek(self, dayOfWeek):
-        if type(dayOfWeek) is Qt.DayOfWeek:
-            self.calender.setFirstDayOfWeek(dayOfWeek)
-        elif type(dayOfWeek) is int:
-            if dayOfWeek < 1 or dayOfWeek > 7:
-                raise Exception("Wrong argument, dayOfWeek should be from 1 to 7 (Monday --> Sunday)")
-            self.calender.setFirstDayOfWeek(Qt.DayOfWeek(dayOfWeek))
-        else:
-            raise Exception("Wrong type, dayOfWeek should be either PySide2.QtCore.Qt.DayOf or int (1 --> 7) (Monday --> Sunday)")
+    def setFirstDayOfWeek(self, firstDayOfWeek):
+        self.calender.setFirstDayOfWeek(firstDayOfWeek)
+        # if type(firstDayOfWeek) is Qt.DayOfWeek:
+        #     self.calender.setFirstDayOfWeek(firstDayOfWeek)
+        # elif type(firstDayOfWeek) is int:
+        #     if firstDayOfWeek < 1 or firstDayOfWeek > 7:
+        #         raise Exception("Wrong argument, firstDayOfWeek should be from 1 to 7 (Monday --> Sunday)")
+        #     self.calender.setFirstDayOfWeek(Qt.DayOfWeek(firstDayOfWeek))
+        # else:
+        #     raise Exception("Wrong type, firstDayOfWeek should be either PySide2.QtCore.Qt.DayOf or int (1 --> 7) (Monday --> Sunday)")
 
     def firstDayOfWeek(self):
         self.calender.firstDayOfWeek()
-
 
     def selectedDate(self):
         self.calender.selectedDate()
@@ -104,3 +112,6 @@ class DatePicker(QWidget):
 
     def setMinimumDate(self):
         self.calender.setMinimumDate()
+
+    def __onSelectionChanged__(self):
+        self.selectionChanged.emit()
